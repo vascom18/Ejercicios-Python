bff4c96eb33acadc5bbe4c0580211ca6fa82daf7
vascom18 <andresvazquez45@gmail.com>
2023-06-05 14:18:20 -0300

    primer ejercicio resuelto

---
+++ 01.py
@@ -0,0 +1,11 @@
+"""Escribí un programa que solicite al usuario que ingrese su nombre. El nombre se debe almacenar en una variable llamada nombre. A continuación se debe mostrar en pantalla el texto “Ahora estás en la matrix, [usuario]”, donde “[usuario]” se reemplazará por el nombre que el usuario haya ingresado.
+ 
+Ejemplo de ejecución:
+
+Tu nombre: Patricia
+Ahora estás en la matrix, Patricia"""
+
+nombre=input('tu nombre:')
+
+print('ahora estas en la matrix,' + nombre)
+
