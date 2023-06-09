0792abe1cebd96d9653c3743c14a99ace9308b60
vascom18 <andresvazquez45@gmail.com>
2023-06-09 18:54:41 -0300

    07 resuelto

---
+++ 07.py
@@ -0,0 +1,11 @@
+'''07 - Escribí un programa que solicite al usuario un número y le reste el 15%, almacenando todo en una única variable. A continuación,
+ mostrar el resultado final en pantalla.
+ 
+Ejemplo de ejecución:
+
+Ingresá un número: 260
+Descontando el 15% queda: 221.0'''
+
+num=int(input('ingrese un numero: '))*0.85
+
+print(f'descontando el 15 % queda:{num}')
