ea92b242cb68ba314eac1c7ac74860aa07faa310
vascom18 <andresvazquez45@gmail.com>
2023-06-10 11:41:29 -0300

    08 resuelto

---
+++ 08.py
@@ -0,0 +1,17 @@
+'''08 - Escribí un programa que solicite al usuario el ingreso de dos palabras, las cuales se guardarán en dos variables
+ distintas. A continuación, almacená en una variable la concatenación de la primera palabra, más un espacio, más la segunda 
+ palabra. Mostrá este resultado en pantalla.
+ 
+Ejemplo de ejecución:
+
+Primera palabra: derribando
+Segunda palabra: muros
+derribando muros'''
+
+palabra1 = input('ingrese una palabra: ')
+
+palabra2 = input('ingrese segunda palabra: ')
+
+concatenacion = palabra1+' '+palabra2
+
+print(concatenacion)
